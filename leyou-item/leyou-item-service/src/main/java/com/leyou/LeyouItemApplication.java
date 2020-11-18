75e0d3fd68e718177344bfb5995ad4b77f98b23b
xuzhiqiang <975945156@qq.com>
2020-11-18 17:37:04 +0800

    分类查询及跨域问题

--- leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemApplication.java
@@ -3,13 +3,14 @@ package com.leyou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.leyou.item.mapper") //mapper接口的包扫描包
 public class LeyouItemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LeyouItemApplication.class);
+        SpringApplication.run(LeyouItemApplication.class,args);
     }
-
 }

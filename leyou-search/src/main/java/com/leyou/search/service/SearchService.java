777d65f0fa0e9303e2031c571c5c838db607783a
Xu <975945156@qq.com>
2020-11-28 16:23:30 +0800

    从spu构建goods-拼接all字段

---
+++ leyou-search/src/main/java/com/leyou/search/service/SearchService.java
@@ -0,0 +1,64 @@
+package com.leyou.search.service;
+
+import com.leyou.item.pojo.Brand;
+import com.leyou.item.pojo.Spu;
+import com.leyou.search.client.BrandClient;
+import com.leyou.search.client.CategoryClient;
+import com.leyou.search.client.GoodsClient;
+import com.leyou.search.client.SpecificationClient;
+import com.leyou.search.pojo.Goods;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class SearchService {
+
+    @Autowired
+    private CategoryClient categoryClient;
+
+    @Autowired
+    private BrandClient brandClient;
+
+    @Autowired
+    private GoodsClient goodsClient;
+
+    @Autowired
+    private SpecificationClient specificationClient;
+
+
+    public Goods buildGoods(Spu spu) {
+        Goods goods = new Goods();
+
+//        根据分类的id查询分类名称
+        List<String> names = this.categoryClient.queryNameByIds(Arrays.asList(spu.getCid1(), spu.getCid2(), spu.getCid3()));
+
+//        根据品牌id查询品牌
+        Brand brand = this.brandClient.queryBrandById(spu.getBrandId());
+
+        goods.setId(spu.getId());
+        goods.setCid1(spu.getCid1());
+        goods.setCid2(spu.getCid2());
+        goods.setCid3(spu.getCid3());
+        goods.setBrandId(spu.getBrandId());
+        goods.setCreateTime(spu.getCreateTime());
+        goods.setSubTitle(spu.getSubTitle());
+
+//        拼接all字段，需要分类名称以及品牌名称
+        goods.setAll(spu.getTitle() + " " + StringUtils.join(names," ") + " " + brand.getName());
+//        获取spu下的所有sku的价格
+        goods.setPrice(null);
+//        获取spu下的所有sku,并转化成json字符串
+        goods.setSkus(null);
+//        获取所有查询的规格参数{name；value}；
+        goods.setSpecs(null);
+
+        return goods;
+
+    }
+
+}

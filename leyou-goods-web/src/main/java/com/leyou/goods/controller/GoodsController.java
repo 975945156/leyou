fd8f2606cad931896f5147b611b7dd842263f6b1
Xu <975945156@qq.com>
2020-12-02 12:14:22 +0800

    Thymeleaf实现静态化

--- leyou-goods-web/src/main/java/com/leyou/goods/controller/GoodsController.java
+++ leyou-goods-web/src/main/java/com/leyou/goods/controller/GoodsController.java
@@ -1,5 +1,6 @@
 package com.leyou.goods.controller;
 
+import com.leyou.goods.service.GoodsHtmlService;
 import com.leyou.goods.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,9 @@ public class GoodsController {
     @Autowired
     private GoodsService goodsService;
 
+    @Autowired
+    private GoodsHtmlService goodsHtmlService;
+
     /**
      * 跳转到商品详情页
      * @param model
@@ -24,11 +28,15 @@ public class GoodsController {
      * @return
      */
     @GetMapping("{id}.html")
-    public String toItemPage(Model model, @PathVariable("id")Long id){
+    public String toItemPage(@PathVariable("id")Long id, Model model){
 
+        // 加载所需的数据
         Map<String, Object> map = this.goodsService.loadData(id);
-
+        // 把数据放入数据模型
         model.addAllAttributes(map);
+
+        // 页面静态化
+        this.goodsHtmlService.asyncExcute(id);
 
         return "item";
     }
